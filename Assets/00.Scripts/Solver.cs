6cc9897430831048d927853bec7dd552f4828c14
Hades000 <lsw463@naver.com>
2022-05-27 00:21:51 +0900

    feat: Add FillAlwaysCell Func to Solver

--- Assets/00.Scripts/Solver.cs
+++ Assets/00.Scripts/Solver.cs
@@ -1,12 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Solver : MonoBehaviour
 {
-    public string[] rowHint;
-    public string[] colHint;
-    
+    public string[] inputRowHint;
+    public string[] inputColHint;
     void Start()
     {
         
+    }
+
+    //  ¹«Á¶°Ç Ä¥ÇØÁö´Â Ä­
+    private void FillAlwaysCell(Cell cell, string[] hint)
+    {
     }
 }
