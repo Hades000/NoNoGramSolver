3ac96731262a7dd98ad5a02fb29a89f2c7433ba3
Hades000 <lsw463@naver.com>
2022-05-27 00:49:49 +0900

    feat: Add ChangeStringHint to Solver

--- Assets/00.Scripts/Solver.cs
+++ Assets/00.Scripts/Solver.cs
@@ -11,7 +11,34 @@ public class Solver : MonoBehaviour
     }
 
     //  ¹«Á¶°Ç Ä¥ÇØÁö´Â Ä­
-    private void FillAlwaysCell(Cell cell, string[] hint)
+    private void FillAlwaysCell(Cell cell, string hint)
     {
+    }
+
+    private List<int> ChangeStringHint(string hint)
+    {
+        List<int> tmp = new List<int>();
+
+        if(hint.Length == 0)
+        {
+            Debug.Log("ChangeStringHint Error : Hint Length Null");
+            return null;
+        }
+        else if(hint.Length == 1)
+        {
+            tmp.Add(int.Parse(hint));
+            return tmp;
+        }
+        else
+        {
+            string[] tmpStr = hint.Split(',');
+
+            for(int i = 0; i < tmpStr.Length;i++)
+            {
+                tmp.Add(int.Parse(tmpStr[i]));
+            }
+
+            return tmp;
+        }
     }
 }
