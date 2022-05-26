148428373e19def615df13a15e3284c013056a61
Hades000 <lsw463@naver.com>
2022-05-27 01:09:39 +0900

    refactor: Fix ChangeStringHing in Solver

--- Assets/00.Scripts/Solver.cs
+++ Assets/00.Scripts/Solver.cs
@@ -11,34 +11,22 @@ public class Solver : MonoBehaviour
     }
 
     //  ¹«Á¶°Ç Ä¥ÇØÁö´Â Ä­
-    private void FillAlwaysCell(Cell cell, string hint)
+    private void FillAlwaysCell(string hint)
     {
+        List<int> hints = ChangeStringHint(hint);
     }
 
     private List<int> ChangeStringHint(string hint)
     {
         List<int> tmp = new List<int>();
 
-        if(hint.Length == 0)
-        {
-            Debug.Log("ChangeStringHint Error : Hint Length Null");
-            return null;
-        }
-        else if(hint.Length == 1)
-        {
-            tmp.Add(int.Parse(hint));
-            return tmp;
-        }
-        else
-        {
-            string[] tmpStr = hint.Split(',');
+        string[] tmpStr = hint.Split(',');
 
-            for(int i = 0; i < tmpStr.Length;i++)
-            {
-                tmp.Add(int.Parse(tmpStr[i]));
-            }
-
-            return tmp;
+        for (int i = 0; i < tmpStr.Length; i++)
+        {
+            tmp.Add(int.Parse(tmpStr[i]));
         }
+
+        return tmp;
     }
 }
