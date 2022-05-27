014f972fb654ae86811981a9ef0a3ba661079fa6
Hades000 <lsw463@naver.com>
2022-05-27 21:58:03 +0900

    feat: Add Solve Function for Puzzle Solve

--- Assets/00.Scripts/Solver.cs
+++ Assets/00.Scripts/Solver.cs
@@ -1,14 +1,30 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CHECK_TYPE {ROW, COL}
+public enum CHECK_TYPE { ROW, COL }
 
 public class Solver : MonoBehaviour
 {
     public string[] inputRowHint;
     public string[] inputColHint;
+
     void Start()
     {
+
+    }
+
+    private IEnumerator Solve()
+    {
+        while(true)
+        {
+            for(int i = 0; i < BoardManager.ins.boardSize; i++)
+            {
+                FillAlwaysCell(i,inputRowHint[i],CHECK_TYPE.ROW);
+            }
+
+            yield return new WaitForSecondsRealtime(1f);
+        }
     }
 
     //  ¹«Á¶°Ç Ä¥ÇØÁö´Â Ä­
@@ -18,9 +34,9 @@ public class Solver : MonoBehaviour
         {
             int intHint = int.Parse(hint);
 
-            if(intHint == BoardManager.ins.boardSize)
+            if (intHint == BoardManager.ins.boardSize)
             {
-                BoardManager.ins.ChangeBoardData(idx,CELL_TYPE.O,CHECK_TYPE.ROW);
+                BoardManager.ins.ChangeBoardData(idx, CELL_TYPE.O, CHECK_TYPE.ROW);
             }
         }
         else
