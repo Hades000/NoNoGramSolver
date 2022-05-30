abb44b180bce5688cfef8e5b1ab6772f526a9cc3
Hades000 <lsw463@naver.com>
2022-05-30 18:33:07 +0900

    feat: 힌트 텍스트 생성 함수 임시 구현 완료

--- Assets/00.Scripts/BoardUI.cs
+++ Assets/00.Scripts/BoardUI.cs
@@ -3,15 +3,31 @@ using UnityEngine.UI;
 
 public class BoardUI : MonoBehaviour
 {
-    public GameObject rowHintPrefabs;
-    public GameObject colHintPrefabs;
+    public Text rowHintPrefabs;
+    public Text colHintPrefabs;
 
     private void Start()
     {
-
+        GenerateHintText();
     }
 
     private void GenerateHintText()
     {
+        int boardSize = BoardManager.ins.boardSize;
+        int textSize = 600 / boardSize;
+
+        for(int i = 0; i < boardSize; i++)
+        {
+            Text newRowHint = Instantiate(rowHintPrefabs);
+            newRowHint.rectTransform.sizeDelta = new Vector2(425,textSize);
+            newRowHint.transform.localPosition = new Vector2(-445,-(i*textSize));
+        }
+
+        for(int i = 0 ; i < boardSize; i++)
+        {
+            Text newColHint = Instantiate(rowHintPrefabs);
+            newColHint.rectTransform.sizeDelta = new Vector2(textSize,550);
+            newColHint.transform.localPosition = new Vector2( i * textSize, 590);
+        }
     }
 }
