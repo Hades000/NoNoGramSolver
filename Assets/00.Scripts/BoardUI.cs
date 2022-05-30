a2bbbbdbab89a9843992a6ec9ab86f9ff3efff94
Hades000 <lsw463@naver.com>
2022-05-30 18:18:14 +0900

    feat: 화면에 보여질 UI를 담당하는 스크립트 추가

---
+++ Assets/00.Scripts/BoardUI.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardUI : MonoBehaviour
+{
+    // public GameObject rowHintPrefabs;
+    // public GameObject colHintPrefabs;
+
+    // private void Start()
+    // {
+
+    // }
+
+    // private void GenerateHint(CHECK_TYPE type)
+    // {
+    //     if(type == CHECK_TYPE.ROW)
+    //     {
+
+    //     }
+    //     else
+    //     {
+
+    //     }
+    // }
+}
